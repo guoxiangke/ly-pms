a1a63b4d6bf7b48204907ad58fa41a1ae65353f4
xy <dev@729ly.com>
2024-07-12 11:43:34 -0700

    HasManyThrough

--- app/Nova/LyMeta.php
+++ app/Nova/LyMeta.php
@@ -190,7 +190,7 @@ class LyMeta extends Resource
             BelongsToMany::make(__('Announcers'), 'announcers', Announcer::class)->allowDuplicateRelations(),
             HasMany::make(__('Ly Episodes'), 'ly_items_with_future', LyItem::class),
 
-            HasManyThrough::make('Lts Items')->showOnDetail(),
+            HasManyThrough::make(__('Lts Episodes'), 'ltsItems', LtsItem::class)->showOnDetail(),
         ];
 
         // 动态添加LTS的Meta

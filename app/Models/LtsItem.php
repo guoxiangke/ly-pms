ea7c550e5a9cf5e6da1d989748f2495dfa12854b
xy <dev@729ly.com>
2024-08-12 22:08:36 -0700

    bug  lazy load
    Attempted to lazy load [ltsMeta] on model [App\Models\LtsItem] but lazy loading is disabled.

--- app/Models/LtsItem.php
+++ app/Models/LtsItem.php
@@ -15,6 +15,7 @@ class LtsItem extends Model
     use HasHashId;
     use HasFactory;
     use SoftDeletes;
+    protected $with = ['ltsMeta'];
     // use Searchable;
     protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
     protected $dates = ['created_at', 'updated_at', 'deleted_at', 'play_at'];

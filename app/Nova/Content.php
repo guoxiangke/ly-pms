8b70dc465a3899ae0271c2065883a5926dde037c
xy <dev@729ly.com>
2024-11-20 19:59:58 -0800

    s3 file

--- app/Nova/Content.php
+++ app/Nova/Content.php
@@ -6,9 +6,12 @@ use Illuminate\Http\Request;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Fields\File;
 use Laravel\Nova\Fields\Markdown;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\MorphToMany;
+use Illuminate\Support\Facades\Storage;
 
 class Content extends Resource
 {
@@ -45,19 +48,39 @@ class Content extends Resource
     {
         return [
             ID::make()->sortable(),
-            Text::make('title'),
-            Markdown::make('summary')->hideFromIndex()->alwaysShow(),
+            Text::make('title')->required(),
+            Textarea::make('summary')->hideFromIndex()->alwaysShow(),
             Markdown::make('body')->hideFromIndex(),
-            BelongsTo::make('user'),
-            // MorphMany::make('lyItems'),
-            // MorphMany::make('ltsItems'),
-
-            MorphToMany::make('lyItems'),
-            MorphToMany::make('ltsItems'),
+            BelongsTo::make('user')->default(\Auth::user()->id)->withoutTrashed(),
+            File::make(__('Attachment'), 'attachment')
+                ->acceptedTypes('.pdf')
+                ->store(function (Request $request, $model) {
+                    $s3filePath = $request->attachment->store('/ly/contents/', 's3');
+                    $model
+                        ->addMediaFromDisk($s3filePath, 's3')
+                        ->toMediaCollection();
+                    return [
+                        'attachment' => $s3filePath
+                    ];
+                })->onlyOnForms(),
+            MorphToMany::make(__('LY Episodes'), 'lyItems', LyItem::class),
+            MorphToMany::make(__('LTS Episodes'), 'ltsItems', LtsItem::class),
 
         ];
     }
+    // https://github.com/laravel/nova-issues/issues/2334#issuecomment-709422756
+    protected static function fillFields(NovaRequest $request, $model, $fields)
+        {
+            $fillFields = parent::fillFields($request, $model, $fields);
 
+            // first element should be model object
+            $modelObject = $fillFields[0];
+
+            // remove all attributes do not have relevant columns in model table
+            unset($modelObject->attachment);
+
+            return $fillFields;
+        }
     /**
      * Get the cards available for the request.
      *

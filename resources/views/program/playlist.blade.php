2100f8824f8a8152062d8f5b99ca587c6ce0339f
xy <dev@729ly.com>
2025-03-27 16:03:01 -0700

    update content

--- resources/views/program/playlist.blade.php
+++ resources/views/program/playlist.blade.php
@@ -220,6 +220,9 @@
           </li>
           @endforeach
         </ul>
+        @foreach($lyItem->contents as $content)
+        <div class="mt-4  bg-gray-50 space-y-6 leading-loose max-w-3xl mx-auto shadow-md rounded-lg p-6 text-gray-800 text-lg leading-relaxed font-serif space-y-4 text-justify">{!! Illuminate\Mail\Markdown::parse($content->body) !!}</div>
+        @endforeach
       </div>
     </div>
     @endif

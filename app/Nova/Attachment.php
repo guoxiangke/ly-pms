407ca07e31550d829b08ffda588276571c92b0f8
xy <dev@729ly.com>
2025-06-20 08:51:11 -0700

    update domain

--- app/Nova/Attachment.php
+++ app/Nova/Attachment.php
@@ -61,7 +61,7 @@ class Attachment extends Resource
             // Text::make('description'),
             // Text::make('description'),
 
-            Text::make('', fn() => '<a target="_blank" href="'.Storage::disk('s3')->url($this->path).'" dusk="ComputedField-download-link" tabindex="0" class="cursor-pointer text-gray-500 inline-flex items-center"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="16" height="16" class="inline-block mr-2" role="presentation" view-box="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg><span class="class mt-1">'.__('Download').'</span></a>')->asHtml()->onlyOnIndex(),
+            Text::make('', fn() => '<a target="_blank" href="'. config('pms.cloudfront_domain') . $this->path .'" dusk="ComputedField-download-link" tabindex="0" class="cursor-pointer text-gray-500 inline-flex items-center"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="16" height="16" class="inline-block mr-2" role="presentation" view-box="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg><span class="class mt-1">'.__('Download').'</span></a>')->asHtml()->onlyOnIndex(),
             // BelongsTo::make('user')->default(\Auth::user()->id)->withoutTrashed()->withMeta(['extraAttributes' => ['readonly' => true]]),
 
             MorphToMany::make(__('LY Episodes'), 'lyItems', LyItem::class)->hideFromDetail(fn () => $this->lyItems->isEmpty()),
